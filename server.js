1bd84ee7a76b67fced6fbd7497394af545f98d1a
MehmetiB <Bashkim.Mehmeti@Broadridge.com>
2023-05-04 01:41:52 +0200

    scanner

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express')
 const pug = require('pug')
 const openFF = require('openfoodfacts-nodejs');
 var bodyParser = require('body-parser')
-
+const fs = require("fs");
+var Quagga = require('quagga').default;
 
 //Instanz wird generiert
 const server = express()
@@ -19,7 +20,7 @@ server.use(bodyParser.json({limit: '10mb', extended: true}))
 
 //Routes
 server.get('/', (request, respone) => {
-  //respone.send ("Moin Liebe Kollegen");
+  console.log(request.body)
   respone.render("index", {text: "Jungs"});
 })
 
@@ -31,19 +32,52 @@ server.post("/",(request, response) => {
   console.log(request.body.ID)
   client.getProduct(request.body.ID).then(product =>{
     var obj = product
-    console.log(obj)
-    //var res = JSON.stringify(obj)
-    //response.json({obj})
-
     response.render("output", { obj });
-
   })
 })
 
 server.post("/upload",(request, response) => {
-  var num = request.body
-  console.log(num)
-  return response.end('Done')
+
+  //HANDLE REQUEST
+  var data = request.body
+  var strData = JSON.stringify(data)
+  var base64Data = strData.replace("data:image/png;base64,", "");
+  base64Data = base64Data.replace("frame","");
+  base64Data = base64Data.slice(4,-1);
+
+  // Store Image into Server
+  const buffer = Buffer.from(base64Data, "base64");
+  fs.writeFileSync("image.png", buffer);
+  fs.writeFileSync("image.txt", base64Data);
+
+  //DO STUFF WITH FILE
+  Quagga.decodeSingle({
+    src: "barcode3.png",
+    numOfWorkers: 0,  // Needs to be 0 when used within node
+    inputStream: {
+        size: 640  // restrict input-size to be 800px in width (long-side)
+      },
+    decoder: {
+        readers: ["ean_reader"] // List of active readers
+      },
+      locate: true, 
+    }, function(result) {
+      if(result.codeResult) {
+        console.log("result", result.codeResult.code);
+        client.getProduct(result.codeResult.code).then(product =>{
+          var obj = product
+          //console.log(obj)
+          //var res = JSON.stringify(obj)
+          //response.json({obj})
+      
+          response.render("output", { obj });
+        })
+      } else {
+        console.log("not detected", result);
+      }
+    });
+  //DELETE FILE AND RESPONSE
+  //fs.unlinkSync("image.png");
 })
 
 const projectRouter = require("./routes/projects");
